ac10ba4c0490b571ec10e112368997292266e19f
Alexander Nasokin <alexnas2python@gmail.com>
2020-07-24 08:56:01 +0300

    Get Github repos for profile.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
@@ -293,6 +294,34 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
       await profile.save();
     }
     return res.status(200).json(profile);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Server Error');
+  }
+});
+
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from Github
+// @access  Public
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`,
+      method: 'GET',
+      headers: {
+        'user-agent': 'node.js',
+      },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No Gihub profile found' });
+      }
+
+      res.json(JSON.parse(body));
+    });
   } catch (err) {
     console.error(err.message);
     return res.status(500).send('Server Error');
