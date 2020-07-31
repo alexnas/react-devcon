1b6115e873f054f756d0efec5c6a51d404e7600d
Alexander Nasokin <alexnas2python@gmail.com>
2020-07-31 20:18:07 +0300

    Add Logout action. Reorganize Navbar links.

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,6 +8,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from '../actions/types';
 
 // Load user
@@ -91,4 +92,9 @@ export const login = (email, password) => async (dispatch) => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+// Logout / Clear Profile
+export const logout = () => (dispatch) => {
+  dispatch({ type: LOGOUT });
 };
