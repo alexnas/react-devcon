bba9991e95c51096bb72b9f0fa67e7aba45cfb2b
Alexander Nasokin <alexnas2python@gmail.com>
2020-08-01 12:21:38 +0300

    Use private routing (PrivateRoute component).

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import store from './store';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 import './App.css';
 
 if (localStorage.token) {
@@ -34,6 +36,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
