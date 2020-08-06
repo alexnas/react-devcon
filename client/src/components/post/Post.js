63df321e50d960a7258c34f8108534c00b54a965
Alexander Nasokin <alexnas2python@gmail.com>
2020-08-06 13:42:48 +0300

    Display Comments List. Delete Comment.

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,10 +1,11 @@
 import React, { useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getPost } from '../../actions/posts';
 import PostItem from '../posts/PostItem';
-import { Link } from 'react-router-dom';
+import CommentItem from '../post/CommentItem';
 import CommentForm from './CommentForm';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -21,6 +22,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className='comments'>
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
